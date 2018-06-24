b43b776442f9fbcceebf5757661a260e5a891d09
comp1 <chotom73@gmail.com>
2018-06-24 14:02:38 +0900

    .

--- src/main/scala/progscala2/fp/curry/CurriedFunc.scala
+++ src/main/scala/progscala2/fp/curry/CurriedFunc.scala
@@ -17,6 +17,49 @@ object CurriedFunc extends App {
 
   println(inverse(1.0))
   println(inverse(2.0))
-  println(inverse(2.0)(4))
   //println(inverse(0.0))
+
+  def cat2(s1: String) = (s2: String) => s1 + s2
+
+  val cat2hello = cat2("Hello")
+  println(s"cat2hello : $cat2hello")
+
+  val cat2world = cat2hello("World!")
+  println(s"cat2world : $cat2world")
+
+  //curried 변환 함수
+  def cat3(s1: String, s2: String)= s1 + s2
+
+  val cat3Curried = (cat3 _).curried
+
+  cat3Curried("hello")("world")
+
+  val cat3Uncurried = Function.uncurried(cat3Curried)
+  println(s"cat3Uncurried : $cat3Uncurried")
+  cat3Uncurried("hello", "world")
+
+  val f1: String => String => String = (s1: String) => (s2:String) => s1 + s2
+  val ff1 = Function.uncurried(f1)
+  ff1("hell", "world")
+
+  def multiplier(i: Int)(factor: Int) = i * factor
+
+  val byFive = multiplier(5) _
+  val byTen = multiplier(10) _
+
+  byFive(2)
+  byTen(2)
+
+  def mult(d1: Double, d2: Double, d3: Double) = d1 + d2 + d3
+
+  val d3 = (2.2, 3.3, 4.4)
+
+  mult(d3._1, d3._2, d3._3)
+
+
+  val multTupled = Function.tupled(mult _)
+  multTupled(d3)
+
+  val multUntupled = Function.untupled(multTupled)
+  multUntupled(d3._1, d3._2, d3._3)
 }
---
+++ src/main/scala/progscala2/fp/curry/LiftedFunc.scala
@@ -0,0 +1,18 @@
+package progscala2.fp.curry
+
+object LiftedFunc extends App {
+  val finicky: PartialFunction[String, String] = {
+    case "finicky" => "FINICKY"
+  }
+
+  finicky("finicky")
+
+  finicky("other")
+
+  // 부분함수 Option 적용 (함수를 끌어올리기)
+  val finickyOption = finicky.lift
+
+  finickyOption("finicky")
+
+  finickyOption("other")
+}
